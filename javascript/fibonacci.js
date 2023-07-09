38d362af5c9879a6261a8b53afbbc0ea71376657
Ricardo Guerra <RikkuG491@gmail.com>
2023-07-08 21:36:00 -0400

    Added solution code

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
   // type your code here
+  if(num === 0){
+    return 0
+  }
+  else if(num === 1){
+    return 1
+  }
+  else{
+    fibonacciArray = [0, 1]
+    for(let index = 2; index <= num; index++){
+      fibonacciArray[index] = fibonacciArray[index - 2] + fibonacciArray[index - 1]
+    }
+    return fibonacciArray[fibonacciArray.length - 1]
+  }
 }
 
 if (require.main === module) {
